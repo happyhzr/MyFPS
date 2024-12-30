54fb1d8295bdc24193891b5eec6e774770a7aef2
happyhzr <insisthzr@163.com>
2024-12-30 16:45:56 +0800

    equipment

--- Assets/Scripts/DeviceTrigger.cs
+++ Assets/Scripts/DeviceTrigger.cs
@@ -5,21 +5,26 @@ using UnityEngine;
 public class DeviceTrigger : MonoBehaviour
 {
     [SerializeField] private GameObject[] targets;
+    [SerializeField] private bool requireKey;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
+        if (requireKey && Managers.Inventory.equippedItem != "key")
+        {
+            return;
+        }
         foreach (GameObject target in targets)
         {
             target.SendMessage("Activate");
--- Assets/Scripts/InventoryManager.cs
+++ Assets/Scripts/InventoryManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class InventoryManager : MonoBehaviour, IGameManager
 {
     public ManagerStatus status { get; private set; }
+    public string equippedItem { get; private set; }
 
     private Dictionary<string, int> items;
 
@@ -26,6 +27,53 @@ public class InventoryManager : MonoBehaviour, IGameManager
             items[name] = 1;
         }
         DisplayItems();
+    }
+
+    public List<string> GetItemList()
+    {
+        List<string> list = new List<string>(items.Keys);
+        return list;
+    }
+
+    public int GetItemCount(string name)
+    {
+        if (items.ContainsKey(name))
+        {
+            return items[name];
+        }
+        return 0;
+    }
+
+    public bool EquipItem(string name)
+    {
+        if (items.ContainsKey(name) && equippedItem != name)
+        {
+            equippedItem = name;
+            Debug.Log($"equipped {name}");
+            return true;
+        }
+        equippedItem = null;
+        Debug.Log("unequipped");
+        return false;
+    }
+
+    public bool ConsumeItem(string name)
+    {
+        if (items.ContainsKey(name))
+        {
+            items[name]--;
+            if (items[name] == 0)
+            {
+                items.Remove(name);
+            }
+        }
+        else
+        {
+            Debug.Log($"cannot consume {name}");
+            return false;
+        }
+        DisplayItems();
+        return true;
     }
 
     // Start is called before the first frame update
