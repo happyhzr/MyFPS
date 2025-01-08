a0f63192e74d03e32523f30f88a6f1aa56e4415a
happyhzr <insisthzr@163.com>
2025-01-08 17:40:10 +0800

    bgm

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -5,10 +5,54 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour, IGameManager
 {
     [SerializeField] private AudioSource soundSource;
+    [SerializeField] private AudioSource music1Source;
+    [SerializeField] private AudioSource music2Source;
+    [SerializeField] private string introBgmMusic;
+    [SerializeField] private string levelBgmMusic;
+    [SerializeField] private float crossFadeRate = 1.5f;
 
     public ManagerStatus status { get; private set; }
 
+    private AudioSource activeMusic;
+    private AudioSource inactiveMusic;
+    private bool crossFading;
+
+    public float musicVolume
+    {
+        get { return _musicVolume; }
+        set
+        {
+            _musicVolume = value;
+            if (music1Source != null && !crossFading)
+            {
+                music1Source.volume = value;
+                music2Source.volume = value;
+            }
+        }
+    }
+
     private NetworkService network;
+    private float _musicVolume;
+
+    public bool musicMute
+    {
+        get
+        {
+            if (music1Source != null)
+            {
+                return music1Source.mute;
+            }
+            return false;
+        }
+        set
+        {
+            if (music1Source != null)
+            {
+                music1Source.mute = value;
+                music2Source.mute = value;
+            }
+        }
+    }
 
     public void Startup(NetworkService service)
     {
@@ -16,6 +60,13 @@ public class AudioManager : MonoBehaviour, IGameManager
         network = service;
         status = ManagerStatus.Started;
         soundVolume = 1f;
+        musicVolume = 1f;
+        music1Source.ignoreListenerVolume = true;
+        music1Source.ignoreListenerPause = true;
+        music2Source.ignoreListenerVolume = true;
+        music2Source.ignoreListenerPause = true;
+        activeMusic = music1Source;
+        inactiveMusic = music2Source;
     }
 
     public float soundVolume
@@ -35,6 +86,22 @@ public class AudioManager : MonoBehaviour, IGameManager
         soundSource.PlayOneShot(clip);
     }
 
+    public void PlayIntroMusic()
+    {
+        PlayMusic(Resources.Load<AudioClip>($"Music/{introBgmMusic}"));
+    }
+
+    public void PlayLevelMusic()
+    {
+        PlayMusic(Resources.Load<AudioClip>($"Music/{levelBgmMusic}"));
+    }
+
+    public void StopMusic()
+    {
+        music1Source.Stop();
+        music2Source.Stop();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -45,5 +112,39 @@ public class AudioManager : MonoBehaviour, IGameManager
     void Update()
     {
 
+    }
+
+    private void PlayMusic(AudioClip clip)
+    {
+        if (crossFading)
+        {
+            return;
+        }
+        StartCoroutine(CrossFadeMusic(clip));
+    }
+
+    private IEnumerator CrossFadeMusic(AudioClip clip)
+    {
+        crossFading = true;
+        inactiveMusic.clip = clip;
+        inactiveMusic.volume = 0;
+        inactiveMusic.Play();
+        float scaledRate = crossFadeRate * _musicVolume;
+        while (activeMusic.volume > 0)
+        {
+            activeMusic.volume -= scaledRate * Time.deltaTime;
+            inactiveMusic.volume += scaledRate * Time.deltaTime;
+            Debug.Log($"active: {activeMusic.volume}");
+            Debug.Log($"inactive: {inactiveMusic.volume}");
+            yield return null;
+        }
+
+        AudioSource temp = activeMusic;
+        activeMusic = inactiveMusic;
+        inactiveMusic = temp;
+        activeMusic.volume = musicVolume;
+        Debug.Log($"music volume: {musicVolume}");
+        inactiveMusic.Stop();
+        crossFading = false;
     }
 }
--- Assets/Scripts/SettingsPopup.cs
+++ Assets/Scripts/SettingsPopup.cs
@@ -50,4 +50,32 @@ public class SettingsPopup : MonoBehaviour
     {
         Messenger<float>.Broadcast(GameEvent.SPEED_CHANGED, speed);
     }
+
+    public void OnPlayMusic(int selector)
+    {
+        Managers.Audio.PlaySound(sound);
+        switch (selector)
+        {
+            case 1:
+                Managers.Audio.PlayIntroMusic();
+                break;
+            case 2:
+                Managers.Audio.PlayLevelMusic();
+                break;
+            default:
+                Managers.Audio.StopMusic();
+                break;
+        }
+    }
+
+    public void OnMusicToggle()
+    {
+        Managers.Audio.musicMute = !Managers.Audio.musicMute;
+        Managers.Audio.PlaySound(sound);
+    }
+
+    public void OnMusicValue(float volume)
+    {
+        Managers.Audio.musicVolume = volume;
+    }
 }
